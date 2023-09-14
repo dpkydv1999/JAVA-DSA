a5d105ea803c17742f21a6481a190927a69450a9
dpkydv1999 <ydv.dpk99@gmail.com>
2023-09-14 18:04:25 +0545

    Duplicate

---
+++ ComplexityAnalysisAndArray/Duplicate.java
@@ -0,0 +1,16 @@
+public class Duplicate {
+
+    public static void main(String[] args) {
+        int[] arr = {2,3,3,6};
+        int n = arr.length;
+
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(arr[i]==arr[j]){
+                    System.out.println("Duplicate element is: "+arr[i]);
+                }
+            }
+        }
+    }
+    
+}
---
+++ ComplexityAnalysisAndArray/Missing.java
@@ -0,0 +1,20 @@
+public class Missing {
+
+   public static void main(String[] args) {
+    int[] arr = {1,2,3,5,6,7};
+    int n= arr.length;
+
+    //sum of natural numbers in an array
+    int sum_natural_num = ((n+1)*(n+2))/2;
+    int sum = 0;
+
+    //sum of current elements present in an array
+    for(int i=0; i<n; i++){
+        sum += arr[i];
+
+    }
+    int missing_elem = 0;
+    missing_elem = sum_natural_num - sum;
+    System.out.println("Missing number is:" +missing_elem);
+   } 
+}

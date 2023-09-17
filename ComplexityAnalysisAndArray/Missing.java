7a344d675be9b1272a210815b9e2297ebad5c6ff
dpkydv1999 <ydv.dpk99@gmail.com>
2023-09-17 18:07:15 +0545

    PrefixSum

--- ComplexityAnalysisAndArray/Missing.java
+++ ComplexityAnalysisAndArray/Missing.java
@@ -1,14 +1,17 @@
+// Time complexity : O(n)
+// Space Complexity : O(1)
+
 public class Missing {
 
    public static void main(String[] args) {
     int[] arr = {1,2,3,5,6,7};
     int n= arr.length;
 
-    //sum of natural numbers in an array
+    //sum of natural numbers in an array - O(1)
     int sum_natural_num = ((n+1)*(n+2))/2;
     int sum = 0;
 
-    //sum of current elements present in an array
+    //sum of current elements present in an array - O(n)
     for(int i=0; i<n; i++){
         sum += arr[i];
 
